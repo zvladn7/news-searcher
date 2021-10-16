c3951931edb15d07e3bd13c8cf48524a1d13971a
zvladn7 <zybkin11@gmail.com>
2021-10-16 12:31:41 +0300

    #1 init backend

---
+++ src/test/java/ru/spbstu/news/searcher/NewsSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spbstu.news.searcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
