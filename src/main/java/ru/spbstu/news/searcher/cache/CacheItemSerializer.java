c7d03a004e9d54702d438dbc4344815547fa3026
vladislav.zybkin <vladislav.zybkin@corp.mail.ru>
2021-11-04 20:19:42 +0300

    remove import

--- src/main/java/ru/spbstu/news/searcher/cache/CacheItemSerializer.java
+++ src/main/java/ru/spbstu/news/searcher/cache/CacheItemSerializer.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
