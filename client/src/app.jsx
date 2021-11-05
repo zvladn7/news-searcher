1a202539fc9bcf322dbe0f01df1038d55b78aecf
Alexander Rakhmatullin <rar130600@mail.ru>
2021-11-05 21:24:57 +0300

    #10 added Ui-Kit page and init frontend project folders

---
+++ client/src/app.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import './styles/index.scss';
+
+import UiKit from "./pages/ui-kit";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/ui-kit'>
+          <UiKit />
+        </Route>
+        <Route path='/'>
+          <div>Main</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
