3f7521bc4dc4516275b40220f1259a9870d00192
ilya.gusarov <ilya.gusarov@corp.mail.ru>
2021-11-15 01:15:09 +0300

    #22 review fix

--- src/main/java/ru/spbstu/news/searcher/database/SearchResultRepository.java
+++ src/main/java/ru/spbstu/news/searcher/database/SearchResultRepository.java
@@ -2,8 +2,5 @@ package ru.spbstu.news.searcher.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SearchResultRepository extends JpaRepository<SearchResult, Long> {
-    Optional<SearchResult> findById(Long id);
 }
