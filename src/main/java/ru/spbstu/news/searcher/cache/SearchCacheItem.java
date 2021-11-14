f768655ae979e8834a86126c286e8744bc98ca22
vladislav.zybkin <vladislav.zybkin@corp.mail.ru>
2021-11-14 23:51:18 +0300

    review fixes

--- src/main/java/ru/spbstu/news/searcher/cache/SearchCacheItem.java
+++ src/main/java/ru/spbstu/news/searcher/cache/SearchCacheItem.java
@@ -1,56 +1,19 @@
 package ru.spbstu.news.searcher.cache;
 
-import org.apache.commons.lang3.Validate;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SearchCacheItem {
 
     private String title;
     private String url;
     private List<String> imageUrls;
 
-    public SearchCacheItem() {
-    }
-
-    public SearchCacheItem(@NotNull String title,
-                           @NotNull String url,
-                           @Nullable List<String> imageUrls) {
-        Validate.notNull(title);
-        Validate.notNull(url);
-        this.title = title;
-        this.url = url;
-        this.imageUrls = imageUrls;
-    }
-
-    @NotNull
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NotNull String title) {
-        Validate.notNull(title);
-        this.title = title;
-    }
-
-    @NotNull
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(@NotNull String url) {
-        Validate.notNull(url);
-        this.url = url;
-    }
-
-    @Nullable
-    public List<String> getImageUrls() {
-        return imageUrls;
-    }
-
-    public void setImageUrls(@Nullable List<String> imageUrls) {
-        this.imageUrls = imageUrls;
-    }
 }
