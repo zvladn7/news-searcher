c7c1e108443651d21644c02f589b58d0a977c262
Alexander Rakhmatullin <rar130600@mail.ru>
2021-11-14 19:20:34 +0300

    #9 added default image

--- client/src/components/ui/imageBlock/index.jsx
+++ client/src/components/ui/imageBlock/index.jsx
@@ -7,7 +7,7 @@ const ImageBlock = (props) => {
 
   return (
     <a className="image-block" href={link} target="_blank">
-      <img className="image-block__image" src={imageUrl} alt={`Image from ${link}`} />
+      <img className="image-block__image" src={imageUrl || defaultImage} alt={`Image from ${link}`} />
       <div className="image-block__bottom-bar">
         <span className="image-block__title">{title}</span>
         <span className="image-block__link">{link}</span>
